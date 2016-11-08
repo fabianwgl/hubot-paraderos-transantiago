03d3f29bdc90d83a250b2c1ea7a26fb64c658f4f
Fabian General <fabianwgl@gmail.com>
2016-11-08 12:35:47 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# hubot-paraderos-transantiago
+
+Informa la disponibilidad de recorridos y el tiempo de llegada de un determinado paradero
+
+See [`src/paraderos-transantiago.coffee`](src/paraderos-transantiago.coffee) for full documentation.
+
+## Installation
+
+In hubot project repo, run:
+
+`npm install hubot-paraderos-transantiago --save`
+
+Then add **hubot-paraderos-transantiago** to your `external-scripts.json`:
+
+```json
+[
+  "hubot-paraderos-transantiago"
+]
+```
+
+## Sample Interaction
+
+```
+user1>> hubot hello
+hubot>> hello!
+```
+
+## NPM Module
+
+https://www.npmjs.com/package/hubot-paraderos-transantiago
---
+++ src/paraderos-transantiago.js
@@ -0,0 +1,40 @@
+// Description
+//   Informa la disponibilidad de recorridos y el tiempo de llegada de un determinado paradero
+//
+// Commands:
+//   hubot tstop [codigo_paradero] <devuelve los recorridos cercanos>
+//
+// Author:
+//   Fabian General <fabianwgl@gmail.com>
+
+var tsapi = require("transantiago-client");
+
+module.exports = function(robot) {
+
+  robot.hear(/tstop (.*)/, function(res){
+    var paradero = escape(res.match[1]);
+    if(paradero!="" || paradero!=null){
+      tsapi(paradero).then( r => {
+      var json = JSON.stringify(r);
+      var respuesta = "";
+
+      for(var i in r.avail){
+
+        respuesta += "Servicio: "+r.avail[i].service;
+        for(var j in r.avail[i].buses){
+
+          respuesta += "\n";
+          respuesta += "BUS: "+r.avail[i].buses[j].bus+" ";
+          respuesta += "LLEGADA: "+r.avail[i].buses[j].arrivaltime+" ";
+          respuesta += "DISTANCIA: "+r.avail[i].buses[j].dist+" ";
+        }
+        respuesta += " \n";
+      }
+
+        return res.send(respuesta);
+      }).catch(err => {
+        return res.send(err);
+      });
+    }
+  });
+}
